4cdb69d14c432e3386f25dd2aa931f14dec90e3e
jnecjnec <jnecjnec@gmail.com>
2017-11-07 19:58:12 +0100

    visual app

--- src/main/java/cz/unicode/gastro/FXMLController.java
+++ src/main/java/cz/unicode/gastro/FXMLController.java
@@ -1,44 +1,92 @@
 package cz.unicode.gastro;
 
+import static cz.unicode.gastro.MainApp.injector;
+import cz.unicode.gastro.gastroManager.gastroManager;
+import cz.unicode.gastro.tcpip.tcpipclient.tcpipclient;
+import cz.unicode.gastro.tcpip.tcpipserver.tcpipserver;
 import java.net.URL;
 import java.util.ResourceBundle;
 import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Label;
 import javafx.scene.control.RadioButton;
+import javafx.stage.WindowEvent;
 
 public class FXMLController implements Initializable {
-    
+
+    tcpipserver server = null;
+
+    tcpipclient client = null;
+
+    gastroManager gastromanager = null;
+
     @FXML
     private Label label;
-    
+
     @FXML
     private RadioButton radioButtonServer;
-    
+
     @FXML
     private RadioButton radioButtonClient;
-    
+
     @FXML
     private void handleButtonAction(ActionEvent event) {
-        startService(true);
-        label.setText("Hello World!");
+        startService(radioButtonServer.selectedProperty().getValue());
     }
-    
+
     @FXML
     private void handleCheckBoxServer(ActionEvent event) {
-      radioButtonServer.selectedProperty().setValue(Boolean.TRUE);
-      radioButtonClient.selectedProperty().setValue(Boolean.FALSE);
+        radioButtonServer.selectedProperty().setValue(Boolean.TRUE);
+        radioButtonClient.selectedProperty().setValue(Boolean.FALSE);
     }
-    
+
     @FXML
     private void handleCheckBoxClient(ActionEvent event) {
-       radioButtonServer.selectedProperty().setValue(Boolean.FALSE);
+        radioButtonServer.selectedProperty().setValue(Boolean.FALSE);
         radioButtonClient.selectedProperty().setValue(Boolean.TRUE);
     }
-    
+
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
-    }    
+        gastromanager = injector.getInstance(gastroManager.class);
+    }
+
+  
+ 
+
+    private void startService(boolean pServer) {
+        if (pServer) {
+            label.setText("Run as server");
+            server = injector.getInstance(tcpipserver.class);
+            server.setListener(gastromanager);
+            server.serverRun();
+
+        } else {
+            label.setText("Run as client");
+            client = injector.getInstance(tcpipclient.class);
+            client.setListener(gastromanager);
+            client.clientRun();
+
+        }
+
+        /*
+        if (!configuration.isServer()) {
+            tableimpl ta = new tableimpl();
+            ta.setUserId(123);
+            gastromanager.addTable(ta);
+        }
+         */
+    }
+    
+    public void shutdown () {
+       if (server != null){
+           server.serverStop();
+       } 
+       if (client != null){
+           client.clientStop();
+       }
+    }
 }
--- src/main/java/cz/unicode/gastro/MainApp.java
+++ src/main/java/cz/unicode/gastro/MainApp.java
@@ -5,11 +5,7 @@ import org.slf4j.Logger;
 import com.google.inject.Guice;
 import com.google.inject.Injector;
 
-import cz.unicode.gastro.gastroManager.gastroManager;
 import cz.unicode.gastro.injector.AppInjector;
-import cz.unicode.gastro.model.table.tableimpl;
-import cz.unicode.gastro.tcpip.tcpipclient.tcpipclient;
-import cz.unicode.gastro.tcpip.tcpipserver.tcpipserver;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
@@ -23,65 +19,42 @@ import static javafx.application.Application.launch;
  */
 public class MainApp extends Application {
 
-        static Injector injector;
-        static tcpipserver server = null;
-        static tcpipclient client = null;
-        static Logger logger;
-        static gastroManager gastromanager = null;
-
-    
-     
-    
+    static Injector injector;
+    static Logger logger;
+  
     @Override
     public void start(Stage stage) throws Exception {
-        Parent root = FXMLLoader.load(getClass().getResource("/fxml/Scene.fxml"));
-
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("/fxml/Scene.fxml"));
+        Parent root = loader.load();
         Scene scene = new Scene(root);
+        FXMLController controller = loader.getController();
 
         stage.setScene(scene);
+      
+        stage.setOnHidden(e -> {
+            controller.shutdown();
+        });
         stage.show();
     }
 
     public static void main(String[] args) {
-         injector = Guice.createInjector(new AppInjector());
-       
-        logger = injector.getInstance(Logger.class);
-    
-        logger.info("Start application");
-        
-         gastromanager = injector.getInstance(gastroManager.class);
-          
-        launch(args);
+        injector = Guice.createInjector(new AppInjector());
 
-    
+        logger = injector.getInstance(Logger.class);
+
+        logger.info("Start application");
+
+        launch(args);
+/*
         if (configuration.isServer()) {
             server.serverStop();
         } else {
             client.clientStop();
         }
-
+*/
         logger.info("Finish application");
-    }
-    
-    public void startService(boolean pServer){
         
-       
-        if (configuration.isServer()) {
-            server = injector.getInstance(tcpipserver.class);
-            server.setListener(gastromanager);
-            server.serverRun();
-        } else {
-            client = injector.getInstance(tcpipclient.class);
-            client.setListener(gastromanager);
-            client.clientRun();
-        }
+    }
 
-        if (!configuration.isServer()) {
-            tableimpl ta = new tableimpl();
-            ta.setUserId(123);
-            gastromanager.addTable(ta);
-        }
-        
-    }
 
 }
